5bccbb26829b7a05836bcbbf87fab4b9f0bd79bb
Nguyen Trung Nam <namnt@i.hivetech.vn>
2025-06-11 11:01:05 +0700

    build:CRUD basic

---
+++ src/main/java/com/example/Identity_service/controller/UserController.java
@@ -0,0 +1,48 @@
+package com.example.Identity_service.controller;
+
+import com.example.Identity_service.dto.request.UserCreationRequest;
+import com.example.Identity_service.dto.request.UserUpdateRequest;
+import com.example.Identity_service.entity.User;
+import com.example.Identity_service.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User creatUser(@RequestBody UserCreationRequest request) {
+        return userService.createUser(request);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{userId}")
+    public User getUser(@PathVariable("userId") String userId) {
+        return userService.getUser(userId);
+    }
+
+    @PutMapping("/{userId}")
+    public User updateUser( @PathVariable("userId") String userId,@RequestBody UserUpdateRequest request) {
+        return userService.updateUser(userId, request);
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable("userId") String userId) {
+         Boolean isDeleted = userService.deleteUser(userId);
+         if (isDeleted) {
+             return "User has been deleted";
+         }else {
+             return "User is not deleted";
+         }
+
+    }
+}
---
+++ src/main/java/com/example/Identity_service/service/UserService.java
@@ -0,0 +1,56 @@
+package com.example.Identity_service.service;
+
+import com.example.Identity_service.dto.request.UserCreationRequest;
+import com.example.Identity_service.dto.request.UserUpdateRequest;
+import com.example.Identity_service.entity.User;
+import com.example.Identity_service.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(UserCreationRequest request) {
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+
+        return userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUser(String id) {
+        return userRepository.findById(id).orElseThrow(()-> new RuntimeException("User not found"));
+    }
+
+    public User updateUser(String id, UserUpdateRequest request) {
+        User user = userRepository.findById(id).orElseThrow(()-> new RuntimeException("User not found"));
+
+        user.setPassword(request.getPassword());
+        user.setFirstName(request.getFirstName());
+        user.setLastName(request.getLastName());
+        user.setDob(request.getDob());
+
+        return userRepository.save(user);
+    }
+
+    public Boolean deleteUser(String id) {
+         if (userRepository.findById(id).isPresent()) {
+             userRepository.deleteById(id);
+             return true;
+         }
+         return false;
+
+    }
+}
